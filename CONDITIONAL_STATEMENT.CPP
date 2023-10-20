9b50f55940ec00be1f71f355927f5b831bd1c908
CRAXY <120028119+ASHWANIKUMARGUPTA14830@users.noreply.github.com>
2023-10-20 23:56:55 +0530

    Add files via upload
    GOOD LEVEL CONTENTS

---
+++ CONDITIONAL_STATEMENT.CPP
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (num > 0) {
+        cout << "The number is positive." << endl;
+    } else if (num < 0) {
+        cout << "The number is negative." << endl;
+    } else {
+        cout << "The number is zero." << endl;
+    }
+
+    return 0;
+}
---
+++ IF-ELSE_STATEMENT.CPP
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+
+    cout << "Enter a number: ";
+    cin >> number;
+
+    if (number % 2 == 0) {
+        cout << "The number is even." << endl;
+    } else {
+        cout << "The number is odd." << endl;
+    }
+
+    return 0;
+}
